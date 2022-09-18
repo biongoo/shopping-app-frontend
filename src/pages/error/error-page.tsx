f30fa92c28c88e21d87e3a22787b62e4697fc445
biongoo <biongoo@wp.pl>
2022-09-18 21:40:52 +0200

    added vite-tsconfig-paths and lottie react

--- src/pages/error/error-page.tsx
+++ src/pages/error/error-page.tsx
@@ -1,21 +1,6 @@
-import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import error from '~/assets/lotties/error.json';
 
 export const ErrorPage = () => {
-  const error = useRouteError();
-
-  if (!isRouteErrorResponse(error)) {
-    return <div>Oops</div>;
-  }
-
-  console.log(error);
-
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error?.statusText}</i>
-      </p>
-    </div>
-  );
+  return <Lottie animationData={error} loop={true} />;
 };
