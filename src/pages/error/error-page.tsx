05f0de843b7f2d2eaaa6031a39d51c061b8809e0
biongoo <biongoo@wp.pl>
2022-09-19 01:05:49 +0200

    added ThemeProvider

--- src/pages/error/error-page.tsx
+++ src/pages/error/error-page.tsx
@@ -1,6 +1,22 @@
 import Lottie from 'lottie-react';
 import error from '~/assets/lotties/error.json';
+import { Stack, Typography } from '@mui/material';
 
 export const ErrorPage = () => {
-  return <Lottie animationData={error} loop={true} />;
+  return (
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={1}
+      height="100vh"
+    >
+      <Lottie
+        animationData={error}
+        loop={true}
+        style={{ maxHeight: '80vh', maxWidth: '95vw' }}
+      />
+      <Typography variant="h6">Go back</Typography>
+    </Stack>
+  );
 };
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,15 +1,43 @@
-import { ReactNode } from 'react';
-import { CssBaseline } from '@mui/material';
+import {
+  createTheme,
+  CssBaseline,
+  PaletteMode,
+  ThemeProvider,
+} from '@mui/material';
+import { amber, deepOrange } from '@mui/material/colors';
+import React, { ReactNode } from 'react';
 
 type Properties = {
   children: ReactNode;
 };
 
+const getDesignTokens = (mode: PaletteMode) => ({
+  palette: {
+    mode,
+    ...(mode === 'light'
+      ? {
+          // light mode
+          primary: amber,
+        }
+      : {
+          // dark mode
+          primary: deepOrange,
+          background: {
+            default: '#161c24',
+            paper: '#161c24',
+          },
+        }),
+  },
+});
+
 export const Theme = ({ children }: Properties) => {
+  const mode = 'dark';
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       {children}
-    </>
+    </ThemeProvider>
   );
 };
