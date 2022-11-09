5f9dcebc36b1075e378e171230859eff84ecd855
biongoo <biongoo@wp.pl>
2022-11-09 22:31:41 +0100

    hotfix

--- src/partials/app-layout/app-layout.tsx
+++ src/partials/app-layout/app-layout.tsx
@@ -1,7 +1,4 @@
-import { Paper } from '@mui/material';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import { Box, Paper, Toolbar } from '@mui/material';
 import { Drawer } from './components';
 
 const drawerWidth = 220;
@@ -18,23 +15,7 @@ export const AppLayout = () => (
       }}
     >
       <Toolbar />
-      <Paper sx={{ p: 2 }}>
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-      </Paper>
+      <Paper sx={{ p: 2 }}>j</Paper>
     </Box>
   </Box>
 );
