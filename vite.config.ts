d09d29ec53cdb19ad33851029933906030caffdb
Dawid Pasionek <biongoo@wp.pl>
2022-12-07 00:43:39 +0100

    updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: path.resolve(
+          path.dirname(fileURLToPath(import.meta.url)),
+          'src'
+        ),
+      },
+    ],
+  },
 });
