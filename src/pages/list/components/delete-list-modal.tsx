2fe5a7cdf227ff6f0ac4272c777e9ab031396e01
Dawid Pasionek <biongoo@wp.pl>
2023-01-04 01:17:30 +0100

    done editing and deleting list

---
+++ src/pages/list/components/delete-list-modal.tsx
@@ -0,0 +1,73 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { deleteList } from '~/api';
+import { AlertModal, TranslatedText } from '~/bits';
+import { QueryKey } from '~/enums';
+import { ApiData } from '~/models';
+import { List, ListPreview } from '~/types';
+import { generateOnError, generateOnSuccess, useClearCache } from '~/utils';
+
+type Props = {
+  isOpen: boolean;
+  list: List;
+  onClose: () => void;
+};
+
+export const DeleteListModal = (props: Props) => {
+  const { list, isOpen, onClose } = props;
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const clearCache = useClearCache(QueryKey.lists);
+
+  const mutation = useMutation({
+    mutationFn: deleteList,
+    onMutate: async (req) => {
+      await queryClient.cancelQueries({
+        queryKey: [QueryKey.lists, req.id],
+      });
+
+      queryClient.setQueryData<ApiData<ListPreview[]>>(
+        [QueryKey.lists],
+        (old) => {
+          if (!old) {
+            return old;
+          }
+
+          return new ApiData(
+            old.status,
+            old.data.filter((x) => x.id !== req.id),
+            old.message
+          );
+        }
+      );
+
+      navigate('/app/home', { replace: true });
+    },
+    onError: generateOnError(),
+    onSuccess: generateOnSuccess({
+      alertTime: 5,
+      message: 'successfullyDeleted',
+      fn: clearCache,
+    }),
+  });
+
+  const handleSubmit = () => {
+    const data = { id: list.id };
+    mutation.mutate(data);
+  };
+
+  return (
+    <AlertModal
+      titleKey="deleteList"
+      isOpen={isOpen}
+      isLoading={mutation.isLoading}
+      onClose={onClose}
+      onOk={handleSubmit}
+    >
+      <TranslatedText
+        textKey="areYouSureDelete"
+        options={{ item: list.name }}
+      />
+    </AlertModal>
+  );
+};
--- src/pages/list/components/modify-list.tsx
+++ src/pages/list/components/modify-list.tsx
@@ -1,35 +1,100 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { IconButton } from '~/bits';
+import { ListItemIcon, MenuItem, Popover } from '@mui/material';
+import { IconButton, TranslatedText } from '~/bits';
 import { ListModal } from '~/partials';
 import { List, ModifyData } from '~/types';
 import { useModal } from '~/utils';
+import { DeleteListModal } from './delete-list-modal';
 
 type Props = {
   list: List;
 };
 
 export const ModifyList = (props: Props) => {
-  const [options, setOpenOptions, setCloseOptions, setHideOptions] =
-    useModal<ModifyData>();
+  const [options, setOpenOptions, setCloseOptions] =
+    useModal<Omit<ModifyData, 'id'>>();
+  const [editModal, setOpenEdit, setCloseEdit] = useModal();
+  const [deleteModal, setOpenDelete, setCloseDelete] = useModal<List>();
 
-  const content = options.isRender ? (
+  const handleOpenEdit = () => {
+    setCloseOptions();
+    setOpenEdit();
+  };
+
+  const handleOpenDelete = () => {
+    setCloseOptions();
+    setOpenDelete(props.list);
+  };
+
+  const optionsContent =
+    options.isRender && options.data ? (
+      <Popover
+        open={options.isOpen}
+        anchorEl={options.data.element}
+        onClose={setCloseOptions}
+        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        PaperProps={{
+          sx: {
+            p: 1,
+            '& .MuiMenuItem-root': {
+              px: 1,
+              typography: 'body2',
+              borderRadius: 0.75,
+            },
+          },
+        }}
+      >
+        <MenuItem onClick={handleOpenEdit}>
+          <ListItemIcon>
+            <EditIcon />
+          </ListItemIcon>
+          <TranslatedText ml={1} textKey="edit" />
+        </MenuItem>
+        <MenuItem onClick={handleOpenDelete}>
+          <ListItemIcon>
+            <DeleteIcon />
+          </ListItemIcon>
+          <TranslatedText ml={1} textKey="delete" />
+        </MenuItem>
+      </Popover>
+    ) : null;
+
+  const editContent = editModal.isRender ? (
     <ListModal
+      isOpen={editModal.isOpen}
       list={props.list}
-      isOpen={options.isOpen}
-      onClose={setCloseOptions}
+      onClose={setCloseEdit}
     />
   ) : null;
+
+  const deleteContent =
+    deleteModal.isRender && deleteModal.data ? (
+      <DeleteListModal
+        isOpen={deleteModal.isOpen}
+        list={deleteModal.data}
+        onClose={setCloseDelete}
+      />
+    ) : null;
 
   return (
     <>
       <IconButton
-        open={options.isOpen}
         titleKey="options"
-        onClick={() => setOpenOptions()}
+        open={options.isOpen}
+        onClick={(e) =>
+          setOpenOptions({
+            element: e.currentTarget,
+          })
+        }
       >
         <MoreVertIcon />
       </IconButton>
-      {content}
+      {optionsContent}
+      {editContent}
+      {deleteContent}
     </>
   );
 };
